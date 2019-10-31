50be0d3a3706c40714a804941ce63e218c1e3122
Christopher Kelling <cj.kelling88@gmail.com>
2019-10-31 14:46:30 -0600

    Create course show page. Also created a method called ordered_grades that orders students by their grades, highest to lowest.

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,8 @@ class Course < ApplicationRecord
   has_many :students, through: :course_students
 
   validates_presence_of :name
+
+  def ordered_grades
+    course_students.joins(:student).select('students.name, course_students.grade, course_students.id').order('course_students.grade DESC')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 
 Rails.application.routes.draw do
   resources :students, only: %i[show]
+  resources :courses, only: %i[show]
 end
