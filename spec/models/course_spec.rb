a588ac9dadf75f4b6c11815a1bd3db7e4d10d88f
Christopher Kelling <cj.kelling88@gmail.com>
2019-10-31 14:02:47 -0600

    Model tests that validates relationships and presence of for course, students, and course_students.

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Course, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should have_many :course_students }
+    it { should have_many(:students).through(:course_students) }
+  end
+end
---
+++ spec/models/student_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Student, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  describe 'relationships' do
+    it { should have_many :course_students }
+    it { should have_many(:courses).through(:course_students) }
+  end
+end
